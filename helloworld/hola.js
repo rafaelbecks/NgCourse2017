f1df883a0347d2fba27b3ce178197868a15e0a3c
Rafael <rafaelbecks93@gmail.com>
2017-11-09 16:02:30 -0400

    ejercicios

---
+++ helloworld/hola.js
@@ -0,0 +1,12 @@
+var Greeter = (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        return "<h1>" + this.greeting + "</h1>";
+    };
+    return Greeter;
+}());
+;
+var greeter = new Greeter("Primer codigo en typescript!");
+document.body.innerHTML = greeter.greet();
---
+++ helloworld/hola.ts
@@ -0,0 +1,11 @@
+class Greeter {
+    constructor(public greeting: string) { 
+    }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Primer codigo en typescript!");
+    
+document.body.innerHTML = greeter.greet();
