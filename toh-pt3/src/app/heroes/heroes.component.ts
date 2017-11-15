dfc4440a71faa2725ee5dc02b913aaf56979a5b7
Rafael <rafaelbecks93@gmail.com>
2017-11-15 16:07:45 -0400

    actualizado ejercicio con servicios y promesa

--- toh-pt3/src/app/heroes/heroes.component.ts
+++ toh-pt3/src/app/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes().then(
+      (heroe) => 
+      {
+        this.heroes = heroe;
+      }
+    );
   }
 
   onSelect(hero: Hero): void {
